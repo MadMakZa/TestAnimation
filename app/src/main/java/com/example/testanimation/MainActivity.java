50471d798b6ab95f37bfbea71bdb73797c6e36a8
MadMakZa <immakza@gmail.com>
2021-01-20 22:14:21 +0300

    create animation listener

--- app/src/main/java/com/example/testanimation/MainActivity.java
+++ app/src/main/java/com/example/testanimation/MainActivity.java
@@ -11,9 +11,13 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,20 +43,41 @@ public class MainActivity extends AppCompatActivity {
     }
     public class Dice {
         public void animationFirstObject() {
-            Double generateX = (Double) (0 + Math.random() * 1000);
-            Double generateY = (Double) (500 + Math.random() * 1000);
-            float xEnd = generateX.floatValue();
-            float yEnd = generateY.floatValue();
+//            Double generateX = (Double) (0 + Math.random() * 1000);
+//            Double generateY = (Double) (500 + Math.random() * 1000);
+//            float xEnd = generateX.floatValue();
+//            float yEnd = generateY.floatValue();
+//
+//            AnimatorSet image1 = new AnimatorSet();
+//            animationX = ObjectAnimator.ofFloat(imageView, "x", -300f, xEnd);
+//            animationY = ObjectAnimator.ofFloat(imageView, "y", -300f, yEnd);
+//            rotateAnimation = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 720f);
+//            animationX.setDuration(1000);
+//            animationY.setDuration(1000);
+//            rotateAnimation.setDuration(1500);
+//            image1.playTogether(animationX, animationY, rotateAnimation);
+//            image1.start();
+            RotateAnimation rotate = new RotateAnimation(5,800,RotateAnimation.RELATIVE_TO_SELF,
+                    0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+            rotate.setDuration(3600);
+            rotate.setFillAfter(true);
+            rotate.setInterpolator(new DecelerateInterpolator());
+            rotate.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    Toast.makeText(getApplicationContext(),"On animation start",Toast.LENGTH_SHORT).show();
+                }
 
-            AnimatorSet image1 = new AnimatorSet();
-            animationX = ObjectAnimator.ofFloat(imageView, "x", -300f, xEnd);
-            animationY = ObjectAnimator.ofFloat(imageView, "y", -300f, yEnd);
-            rotateAnimation = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 720f);
-            animationX.setDuration(1000);
-            animationY.setDuration(1000);
-            rotateAnimation.setDuration(1500);
-            image1.playTogether(animationX, animationY, rotateAnimation);
-            image1.start();
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    Toast.makeText(getApplicationContext(),"On animation end",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                    Toast.makeText(getApplicationContext(),"On animation repeat",Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         public void animationTwoObject() {
             Double generateX = (Double) (0 + Math.random() * 1000);
